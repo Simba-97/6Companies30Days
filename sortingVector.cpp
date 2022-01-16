eab74e16d124bb1438a467edda05d37cb659538e
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-01-16 22:27:35 +0530

    Recursion

---
+++ sortingVector.cpp
@@ -0,0 +1,39 @@
+//sorting an array using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+void insert(vector<int> &v, int temp){
+    if(v.size() == 0 || v[v.size()-1] <= temp){
+        v.push_back(temp);
+        return;
+    }
+
+    int val = v[v.size()-1];
+    v.pop_back();
+    insert(v, temp);
+    v.push_back(val);
+    return;
+}
+
+void sortArray(vector<int> &v) {
+    if(v.size() == 1) return;
+
+    int temp = v[v.size()-1];
+    v.pop_back();
+    sortArray(v);
+    insert(v, temp);
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    sortArray(v);
+
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+}
